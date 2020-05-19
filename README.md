cb6b24f2387349a7484325247337e90843474105
Tobias Kunze <r@rixx.de>
2020-05-19 14:14:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# stepmania-to-sqlite
+
+[![PyPI](https://img.shields.io/pypi/v/stepmania-to-sqlite.svg)](https://pypi.org/project/stepmania-to-sqlite/)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://github.com/rixx/stepmania-to-sqlite/blob/master/LICENSE)
+
+Put stats about your stepmania library into a SQLite database. Will export all songs with data about their groups,
+difficulty levels, durations, bpms, etc. Particularly useful if you use the database with
+[datasette](https://datasette.readthedocs.io/).
+
+## How to install
+
+    $ pip install stepmania-to-sqlite
+
+Add the `-U` flag to update. Change notes can be found in the ``CHANGELOG`` file, next to this README.
+
+## Importing data
+
+Run the tool with the path to your database - if it doesn't exist yet, it will be created:
+
+    $ stepmania-to-sqlite songs.db
+
+If your library is not located in a directory like ``~/.stepmania*``, you can pass the library path with the -l flag:
+
+    $ stepmania-to-sqlite songs.db -l /path/to/library
+    
+## Limitations
+
+- Steps are not included, only pre-processed step counts per difficulty.
+
+## Thanks
+
+This package is heavily inspired by [github-to-sqlite](https://github.com/dogsheep/github-to-sqlite/) by [Simon
+Willison](https://simonwillison.net/2019/Oct/7/dogsheep/).
---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+
+from setuptools import setup
+
+VERSION = "0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="stepmania-to-sqlite",
+    description="Save data about your Stepmania library to a SQLite database",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Tobias Kunze",
+    author_email="r@rixx.de",
+    url="https://github.com/rixx/stepmania-to-sqlite",
+    project_urls={
+        "Source": "https://github.com/rixx/stepmania-to-sqlite",
+        "Issues": "https://github.com/rixx/stepmania-to-sqlite/issues",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Database",
+    ],
+    keywords="stepmania dance sqlite export dogsheep",
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["stepmania_to_sqlite"],
+    entry_points="""
+        [console_scripts]
+        stepmania-to-sqlite=stepmania_to_sqlite.cli:cli
+    """,
+    install_requires=[
+        "beautifulsoup4~=4.8",
+        "click",
+        "python-dateutil",
+        "requests",
+        "sqlite-utils~=2.4.4",
+        "tqdm~=4.36",
+    ],
+)
